4fdaecf8714883749ae400ef017e4a8ed69e05a8
VictorWanjala <wanjala94vic@gmail.com>
2023-04-15 21:32:56 +0300

    Part3

--- app/src/main/java/com/vicvestar/rentcollectionapp/DashboardActivity.kt
+++ app/src/main/java/com/vicvestar/rentcollectionapp/DashboardActivity.kt
@@ -2,6 +2,8 @@ package com.vicvestar.rentcollectionapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.net.Uri
+import androidx.cardview.widget.CardView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
@@ -14,6 +16,11 @@ class DashboardActivity:AppCompatActivity() {
         PayRent.setOnClickListener {
             val intent = Intent(this, PayActivity::class.java)
             startActivity(intent)
+        }
+        Call.setOnClickListener {
+            val intent =  Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:0700794573"));
+            startActivity(intent);
         }
     }
 }
